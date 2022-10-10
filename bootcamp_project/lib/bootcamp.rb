fdf49bbaa560983eed7806dff757fed531be2041
Morgan Rector <mmrector@yahoo.com>
2022-10-10 15:04:34 -0700

    part1

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,50 @@
+#should accept a name (string), slogan (string), and student_capacity (number) as args
+
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+
+        @teachers = []
+        @students = []
+        @grades = Hash.new {|hash, k| hash[k] = []}
+    end 
+
+    def name
+        @name
+    end 
+
+    def slogan
+        @slogan
+    end 
+
+    def teachers
+        @teachers
+    end
+    
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        teachers << teacher
+    end 
+
+    def enroll(student)
+        if students.length < @student_capacity
+            @students << student
+            return true
+        else
+            return false
+        end 
+    end 
+
+    def enrolled?(student)
+        if @students.include?(student)
+            return true
+        else
+            false
+        end
+    end 
 end
