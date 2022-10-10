b80c1a9d5d65deeb6dc63ff1c65c73d6100e8b7b
Morgan Rector <mmrector@yahoo.com>
2022-10-10 14:45:50 -0700

    complete

--- dog_project_morgan_rector/lib/dog.rb
+++ dog_project_morgan_rector/lib/dog.rb
@@ -1,3 +1,48 @@
+#should accept a name (string), breed (string), age (number), bark (string), and favorite_foods (array) as arguments
+
 class Dog
+    def initialize(name,breed,age,bark,favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods= favorite_foods
+    end 
+
+    def name
+        @name
+    end 
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(number)
+        @age=number
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end 
+
+    def bark
+        if @age > 3
+             @bark.upcase
+        else
+             @bark.downcase
+        end 
+    end 
+
+    def favorite_food?(str)
+        str = str.downcase
+        @favorite_foods = @favorite_foods.map! {|ele| ele.downcase}
+        return true if @favorite_foods.include?(str.downcase)
+        false
+    end 
 
 end
+
