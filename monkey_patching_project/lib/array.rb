e552d88b9e54e94aab5ecd2b8fa4f16e3ef48c2f
Morgan Rector <mmrector@yahoo.com>
2022-10-10 17:21:01 -0700

    completed_monkey

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -39,4 +39,65 @@ class Array
         end 
         count_hash
     end
+
+
+    def my_count(val)
+        count = 0
+
+        self.each do |ele|
+            if ele == val
+                count +=1
+            end
+        end 
+        count
+    end 
+
+    def my_index(val)
+
+        self.each_with_index do |ele, i|
+            if val == ele
+               return i
+            end
+        end 
+        nil
+    end 
+
+    def my_uniq
+        #define a return array
+        uniq = []
+        # loop through array
+            self.each do |ele|
+        # if return array does not include the element, push it
+                if !uniq.include?(ele)
+                    uniq << ele
+                end 
+            end
+        uniq
+    end 
+
+    # # arr_1 = [
+    #       ["a", "b", "c"],
+    #       ["d", "e", "f"],
+    #       ["g", "h", "i"]
+    #     ]
+    #     expected_1 = [
+    #       ["a", "d", "g"],
+    #       ["b", "e", "h"],
+    #       ["c", "f", "i"]
+    #     ]
+
+    def my_transpose
+        transposed = Array.new(self.length) {|ele| ele = []}
+
+        self.each_with_index do |subarr, i|
+            subarr.each_with_index do |ele, j|
+                transposed[i][j] = self[j][i]
+            end
+        end 
+        transposed
+    end 
+
+
+
+    #final end statement
 end
